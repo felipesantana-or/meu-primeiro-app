b3963ce5e815210a84fad16c3b5f04bfb0399cc3
Felipe Santana Pereira <taurinafairona@gamil.com>
2026-08-06 11:09:57 -0300

    signal

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
+ 
+
 
 @Component({
   selector: 'app-lista-produtos',
@@ -8,11 +10,17 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //REATIVO QUE PERMITE ALTERAÇÕES (COM SET OU UPDATE)
   produtos = signal([
     { nome: 'Notebook', preco: 3800 },
     { nome: 'Mouse', preco: 179 },
     { nome: 'fone', preco: 80 },
   ]);
+
+  //OBSERVA OUTRO SIGNAL E SE ATUALIZA AUTOMATICAMENTE
+  totalProdutos = computed(() => this.produtos().length);
+
+
 
   exibirProduto(nome: string) {
     console.log('Produto selecionado:', nome);
